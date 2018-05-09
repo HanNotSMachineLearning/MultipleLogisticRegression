adec755c2c2a8fb30b4abac38ca265be40b4c4ed
Robin Bozan <robinbozan@gmail.com>
2018-05-09 11:58:44 +0200

    Voeg hooikoorts toe

--- longontsteking.py
+++ longontsteking.py
@@ -8,6 +8,8 @@ with open('longontsteking_data.csv', 'r') as DataFile:
     csvList = list(csv.reader(DataFile))[1:]
     shuffle(csvList)
     trainDataCount = round(len(csvList) * 7 / 10) + 1
+    print("Using " + str(trainDataCount) + " rows for training")
+    print("Using " + str(len(csvList) - trainDataCount) + " rows for testing")
     trainData = csvList[0:trainDataCount].copy()
     testData = csvList[trainDataCount:].copy()
 
@@ -20,14 +22,13 @@ labels = []
 
 # split labels from features
 for item in testData:
-    test_labels.append(item[:1])
+    test_labels.append(item[-1])
     test_features.append(item[:-1].copy())
 
 for item in trainData:
-    labels.append(item[:1])
+    labels.append(item[-1])
     features.append(item[:-1].copy())
 
-print(labels)
 
 # create a decision tree classifier
 DT_clf = tree.DecisionTreeClassifier()
@@ -37,7 +38,7 @@ DT_clf = DT_clf.fit(features, labels)
 print("\nHello, I am Fruity. \nI can predict what kind of fruit you have. \nFor this I only need to know the height, width and weight.")
 
 while True:
-    print("\nGeslacht? (0 voor MAN, 1 voor VROUW)")
+    print("\nGeslacht? (0 voor VROUW, 1 voor MAN)")
     # get height value from user
     geslacht = int(input(""))
     print("\nLeeftijd?")
@@ -70,13 +71,20 @@ while True:
     snelle_ademhaling = int(input(""))
 
     # predict fruit type on the basis of data entered by the user
-    prediction = DT_clf.predict([[geslacht, leeftijd, hoesten, zere_keel, pijn_op_de_borst,
-                                  kortademigheid, druk_op_de_borstkas, koorts, snelle_ademhaling]])[0]
+    prediction = int(DT_clf.predict([[geslacht, leeftijd, hoesten, zere_keel, pijn_op_de_borst,
+                                      kortademigheid, druk_op_de_borstkas, koorts, snelle_ademhaling]])[0])
     # link correct fruitname to predicted value
     print("\n\nI think you have a:")
-    print("\t\t" + prediction)
-    time.sleep(1)
-    print("\n\n\nDo you want me to predict LONGONTSTEKING again? (Y/N)")
+    if prediction == 1:
+        print("Longontsteking")
+    elif prediction == 2:
+        print("Hooikoorts")
+    else:
+        print("Helemaal niets")
+
+    print(prediction)
+
+    print("\n\n\nDo you want me to predict YOUR ZIEKTE again? (Y/N)")
     again = input("")
     if again.upper() != "Y":
         break
